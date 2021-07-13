866ae5efa22546a56926360f57ef21a4f6e247b6
tailw <tailw@DESKTOP-MR3PNPI>
2021-07-13 16:47:26 -0700

    working on slot machine

---
+++ src/_12_slot_machine/slot_machine_runner.java
@@ -0,0 +1,10 @@
+package _12_slot_machine;
+
+import javax.swing.JFrame;
+
+public class slot_machine_runner {
+	public static void main(String[] args) {
+		slot_machine slot = new slot_machine();
+	}
+	
+}
